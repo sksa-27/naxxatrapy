6e10368ed68f0c6c2f550815fd7dad11f3868ca3
Sagar JC <78749788+SagarJC@users.noreply.github.com>
2021-02-17 19:33:13 +0530

    Add orbital mechanics functions
    - angular displacement
    - angular velocity
    - angular acceleration
    - moment of inertia
    - angular momentum 
    - angular kinetic energy
    - centripetal acceleration 
    - torque

--- naxxatrapy/naxxatrapy.py
+++ naxxatrapy/naxxatrapy.py
@@ -72,30 +72,236 @@ def momentum():
 def force(mass, acceleration=None, velocity=None, time=None):
     pass
 
-
 def gravitational_force():
     pass
 
+def work(force, displacement, theta):
+    """
+       Calculates the work done by 
+       the force while displacing
+       an object at an angle theta
+       
+       Parameters
+       ----------
+       force : float
+       displcement : float 
+       theta : float
+       
+       Returns
+       -------
+       float
+    """
+    return force * displacement * math.cos(theta)
 
-def work():
-    pass
+def power(work, time):
+    """
+       Power is the rate at which the work is done.
+       Calculates the amountof work done divided by 
+       the time it takes to do the work
+       
+       Parameters
+       ----------
+       work : float
+       time : float
+       
+       Returns
+       -------
+       float
+    """
+    return work / time
 
+def kinetic_energy(mass, velocity):
+    """
+       Calculates the energy associated 
+       with an object with mass moving
+       at a given velocity 
+       
+       Parameters
+       ----------
+       mass : float
+       velocity : float
+       
+       Returns
+       -------
+       float
+    """
+    return (1/2) * mass * velocity**2
 
-def power():
-    pass
+def total_energy(T, V):
+    """
+       Total energy of an object is 
+       the sum of its kinetic energy 
+       'T' and potential energy 'V'
+       
+       Parameters
+       ----------
+       T : float
+       V : float
+       
+       Returns
+       -------
+       float
+    """
+    return T + V
 
+def angular_displacement(l, r):
+    """
+       Calculates the angular displacement of
+       an object whose linear displacement is 
+       'l' and radius of the path traced is 'r'
+       
+       Parameters
+       ----------
+       l : float
+       r : float
+       
+       Returns
+       -------
+       float
+    """
+    return l / r
 
-def kinetic_energy():
-    pass
+def angular_velocity(v, r):
+    """
+       Calculates the angular velocity of an
+       object whose linear velocity is 'v'
+       and radius of the path traced is 'r'
+       
+       Parameters
+       ----------
+       v : float
+       r : float
+       
+       Returns
+       -------
+       float
+    """
+    return v / r
 
+def angular_acceleration(a, r):
+    """
+       Calculates the angular acceleration of an
+       object whose linear acceelration is 'a' and 
+       radius of the path traced by the object is 'r'
+      
+       Parameters
+       ----------
+       a : float
+       r : float
+       
+       Returns
+       -------
+       float
+    """
+    return a / r
 
-def total_energy():
-    pass
+def moment_of_inertia(m, k):
+    """
+       Calculates the moment of inertia of an object
+       of mass 'm' with radius of gyration 'k'
+      
+       Parameters
+       ----------
+       m : float
+       k : float
+       
+       Returns
+       -------
+       float
+    """
+    return m * k**2
 
+def angular_momentum(m, v, r, theta):
+    """
+       Calculates the angular momentum of an object 
+       of mass 'm' whose linear velocity is 'v' and 
+       radius of the path traced by the object is 'r',
+       angle between velocity and radius is 'theta'
+      
+       Parameters
+       ----------
+       m : float
+       v : float
+       r : float
+       theta : float
+       
+       Returns
+       -------
+       float
+    """
+    return m * v * r * math.sin(theta) 
 
-def centripetal_force():
-    pass
+def centripetal_acceleration(v, r):
+    """
+       Calculates the centripetal acceleration of
+       an object whose linear velocity is 'v' and 
+       radius of the path traced by the object is 'r'
+      
+       Parameters
+       ----------
+       v : float
+       r : float
+       
+       Returns
+       -------
+       float
+    """
+    return v**2 / r
 
+def centripetal_force(m, v, r):
+    """
+       Centripetal force is the net force
+       that acts on an object with mass 'm' to 
+       keep it moving with a velocity 'v' tracing
+       a circular path of radius 'r'
+       
+       Parameters
+       ----------
+       m : float
+       v : float 
+       r : float
+       
+       Returns
+       -------
+       float
+    """
+    return m * v**2 / r
+
+def angular_kinetic_energy(I, w):
+    """
+       Calculates the angular kinetic energy
+       of an object with moment of inertia 'I'
+       and angular velocity 'w'
+       
+       Parameters
+       ----------
+       I : float
+       w: float 
+       
+       Returns
+       -------
+       float
+    """
+    return (1/2) * I * w**2
+
+def torque(r, F, theta):
+    """
+       Calculates the torque acting on an object
+       where 'r' is the radius, 'F' is the force 
+       and 'theta' is the angle between 'r' and 'F'
+      
+       Parameters
+       ----------
+       m : float
+       v : float
+       r : float
+       theta : float
+       
+       Returns
+       -------
+       float
+    """
+    return  r * F * math.sin(theta)
 
 def friction():
     pass
